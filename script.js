a719c5784cbe1d83f230c34b71eb4c75da1474f3
AOmwenga <aomwenga@gmail.com>
2024-09-06 15:33:48 -0500

    Done with part 2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Part 1: Math Problems
 
-    //Starter Code
-    // The initial numbers that must be verified.
+//Starter Code
+// The initial numbers that must be verified.
 const n1 = 10;
 const n2 = 15;
 const n3 = 20;
@@ -10,7 +10,7 @@ const n4 = 5;
 // Check one: add up to 50
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
-const isSum50 = (n1 + n2 + n3 + n4) == 50;
+const isSum50 = n1 + n2 + n3 + n4 == 50;
 
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
@@ -27,11 +27,12 @@ const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
-const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+const isUnique =
+  n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
 
-// Here, we put the results into a single variable 
+// Here, we put the results into a single variable
 // for convenience. Note how we negate isOver25 using
-// the ! operator. We could also have tested for 
+// the ! operator. We could also have tested for
 // "isUnder25" as an alternative.
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 
@@ -42,9 +43,69 @@ console.log(isValid);
 // but it SHOULD NOT be done this way. As programmers,
 // we break things into small, manageable pieces so that
 // they can be better understood, scaled, and maintained.
-const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
-  ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
-  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
-  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+const dontDoThis =
+  n1 + n2 + n3 + n4 == 50 &&
+  (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2 &&
+  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) &&
+  n1 != n2 &&
+  n1 != n3 &&
+  n1 != n4 &&
+  n2 != n3 &&
+  n2 != n4 &&
+  n3 != n4;
+
+// Check if all numbers are divisible by 5. Cache the result in a variable.
+const divby5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) === 0;
+
+// Check if the first number is larger than the last. Cache the result in a variable.
+const firstlast = n1 > n4;
+
+// Accomplish the following arithmetic chain:
+// Subtract the first number from the second number.
+let firstchain = n1 - n4;
+
+// Multiply the result by the third number.
+firstchain *= n3;
+
+// Find the remainder of dividing the result by the fourth number.
+firstchain %= n4;
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+let isUnder25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
+
 //Part 2 Practical Math
+// The distance of the trip, in total, is 1,500 miles.
+// Your car’s fuel efficiency is as follows:
+// At 55 miles per hour, you get 30 miles per gallon.
+// At 60 miles per hour, you get 28 miles per gallon.
+// At 75 miles per hour, you get 23 miles per gallon.
+// You have a fuel budget of $175.
+// The average cost of fuel is $3 per gallon.
+
+// Set up a program to answer the following questions:
+// How many gallons of fuel will you need for the entire trip?
+
+// didn't read the intire question sorry.
+let distance = 1500;
+let budget = 175;
+let fuelcost = 3;
+// let mph55 = (distance / 30) * fuelcost <= budget;
+// let mph60 = (distance / 28) * fuelcost <= budget;
+// let mph75 = (distance / 23) * fuelcost <= budget;
+
+let mph55 = distance / 30;
+let mph60 = distnace / 28;
+let mph75 = distance / 23;
+
+// Will your budget be enough to cover the fuel expense?
+//true = yes you have enough
+let enoughat55 = mph55 <= budget;
+let enoughat60 = mph60 <= budget;
+let enoughat75 = mph75 <= budget;
+
+// How long will the trip take, in hours?
+let timeAt55 = distance / 55;
+let timeAt60 = distance / 60;
+let timeAt75 = distance / 75;
+
 //Part 3 Future Exploration
